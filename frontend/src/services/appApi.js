bdc930ab2e1cc4755a520f0666e7a4fd47819ae6
HoangAnh <nguyenhoanganh.itc96@gmail.com>
2022-09-18 22:15:38 +0700

    format again project structure

--- frontend/src/services/appApi.js
+++ frontend/src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const appApi = createApi({
   reducerPath: 'appApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://mern-chat-phi.vercel.app',
+    baseUrl: 'http://localhost:5001',
   }),
 
   endpoints: (builder) => ({
--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const app = express();
 
 const rooms = ["general", "tech", "fiance", "crypto"];
 
-if(process.env.NODE_ENV !== "production"){
-  require("dotenv").config({path: __dirname+'/.env'});
-}
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -105,8 +101,8 @@ server.listen(PORT, () => {
 
 // static files (build frontend)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../mern-chat-frontend/build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../mern-chat-frontend/build/index.html'));
+  app.use(express.static(path.join(__dirname, './frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
   })
 }
