b99cd1c00b1301ca052e182966e7633b22ee8c83
HoangAnh <nguyenhoanganh.itc96@gmail.com>
2022-09-18 22:29:25 +0700

    fix deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const userRoutes = require("./routes/useRoutes");
 const Message = require("./models/Message");
 const User = require("./models/User");
